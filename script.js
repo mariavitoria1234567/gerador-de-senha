939e1c8e54e29ba489a439e0dc6c1fbbc92026c2
mariavitoria1234567 <lopes.ramos.maria@escola.pr.gov.br>
2026-09-17 12:38:04 -0300

    gerador de senha

---
+++ script.js
@@ -0,0 +1,33 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto')
+camposenha.volume = document.querySelector(campo-senha)
+
+
+tamanhoSenha = 8
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+
+
+
+botoes[0].onclick = diminuir;
+
+function diminuir(){
+    tamanhoSenha--;
+    numeroSenha.textContent = tamanhoSenha;
+}
+
+//  FUNÇÃO DE CRIAR A SENHA ALEATORIA 
+    gerasenha()
+
+    function gerasenha()
+    let senha = ""
+    //LOOP - repetição 
+    for( let 1 = 0; < tamanhosenha; ++){ 
+        let numeroaleatorio = math.random() 2
+        numeroaleatorio = math.flor(numeroaleatorio)
+        senha = senha + letrasmausculas 
+
+camposenha.volume = senha
+
+    
+    }
